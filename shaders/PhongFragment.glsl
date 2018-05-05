21154db1bb3f6b616500f43dfd5564aab349ceb1
Anastasija3793 <s4923023@bournemouth.ac.uk>
2018-05-05 19:25:18 +0100

    working on deforming

--- shaders/PhongFragment.glsl
+++ shaders/PhongFragment.glsl
@@ -35,10 +35,6 @@ uniform float shininesstest;
 //Noise algorithm from:
 //https://thebookofshaders.com/13/
 //---------------------------------------------------------------------------------------------
-//uniform vec2 u_resolution;
-//uniform vec2 u_mouse;
-//uniform float u_time;
-
 float random (in vec2 st) {
     return fract(sin(dot(st.xy,
                          vec2(12.9898,78.233)))*
@@ -108,3 +104,74 @@ void main() {
     FragColor = vec4(color,1.0);
 
 }
+
+
+
+
+
+//-------------MARBLE-SHADER-TEST----------------------------
+//https://thebookofshaders.com/edit.php?log=161128210559
+//float random (in vec2 st) {
+//    return fract(sin(dot(st.xy,
+//                         vec2(12.9898,78.233)))*
+//        43758.5453123);
+//}
+
+//// Based on Morgan McGuire @morgan3d
+//// https://www.shadertoy.com/view/4dS3Wd
+//float noise (in vec2 st) {
+//    vec2 i = floor(st);
+//    vec2 f = fract(st);
+
+//    // Four corners in 2D of a tile
+//    float a = random(i);
+//    float b = random(i + vec2(1.0, 0.0));
+//    float c = random(i + vec2(0.0, 1.0));
+//    float d = random(i + vec2(1.0, 1.0));
+
+//    vec2 u = f * f * (3.0 - 2.0 * f);
+
+//    return mix(a, b, u.x) +
+//            (c - a)* u.y * (1.0 - u.x) +
+//            (d - b) * u.x * u.y;
+//}
+
+//#define OCTAVES 6
+//float fbm (in vec2 st) {
+//    // Initial values
+//    float value = 0.0;
+//    float amplitud = .5;
+//    float frequency = 0.;
+//    //
+//    // Loop of octaves
+//    for (int i = 0; i < OCTAVES; i++) {
+//        value += amplitud * noise(st);
+//        st *= 2.;
+//        amplitud *= .5;
+//    }
+//    return value;
+//}
+
+//float edge(float v, float center, float edge0, float edge1) {
+//    return 1.0 - smoothstep(edge0, edge1, abs(v - center));
+//}
+
+//void main() {
+////    vec2 st = gl_FragCoord.xy / u_resolution.xy;
+////    st.x *= u_resolution.x / u_resolution.y;
+
+
+//    float v0 = edge(fbm(WSTexCoord * 18.0), 0.5, 0.0, 0.2);
+//    float v1 = smoothstep(0.5, 0.51, fbm(WSTexCoord * 14.0));
+//    float v2 = edge(fbm(WSTexCoord * 14.0), 0.5, 0.0, 0.170);
+//    float v3 = edge(fbm(WSTexCoord * 14.0), 0.5, 0.0, 0.122);
+
+//    vec3 col = vec3(1.000,0.969,0.794);
+//    col -= v0 * 0.230;
+//    col = mix(col, vec3(0.970,0.794,0.609), v1);
+//    col = mix(col, vec3(0.510,0.443,0.366), v2);
+//    col -= v3 * 0.2;
+
+//    FragColor = vec4(col,1.0);
+//}
+//------------------------------------------------------
--- shaders/PhongVertex.glsl
+++ shaders/PhongVertex.glsl
@@ -22,6 +22,52 @@ smooth out vec3 WSVertexPosition;
 smooth out vec3 WSVertexNormal;
 smooth out vec2 WSTexCoord;
 
+//---------------------------------------------------------------------------------------------
+//Noise algorithm from:
+//https://thebookofshaders.com/13/
+//---------------------------------------------------------------------------------------------
+float random (in vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
+// Based on Morgan McGuire @morgan3d
+// https://www.shadertoy.com/view/4dS3Wd
+float noise (in vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+}
+
+#define OCTAVES 6
+float fbm (in vec2 st) {
+    // Initial values
+    float value = 0.0;
+    float amplitude = .5;
+    float frequency = 0.;
+    //
+    // Loop of octaves
+    for (int i = 0; i < OCTAVES; i++) {
+        value += amplitude * noise(st);
+        st *= 2.;
+        amplitude *= .5;
+    }
+    return value;
+}
+//---------------------------------------------------------------------------------------------
+
 void main()
 {
     // Transform the vertex normal by the inverse transpose modelview matrix
@@ -33,6 +79,9 @@ void main()
     // Copy across the texture coordinates
     WSTexCoord = TexCoord;
 
+    vec3 deform = vec3(0.5,0.5,0.8);
+    deform += fbm(TexCoord*3.0); //3.0
+
     // Compute the position of the vertex
-    gl_Position = MVP * vec4(VertexPosition,1.0);
+    gl_Position = MVP * vec4(VertexPosition*deform,1.0);
 }
